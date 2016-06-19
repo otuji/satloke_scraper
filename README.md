41551d15f729b7936bdd390406890be44a7b55e3
otuji <otuji_otuji_otuji@hotmail.com>
2016-06-20 00:57:11 +0900

    とりあえず動作するようになったのでコミット。 取得元を変更するためにはソースの変更が必要

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # satloke_scraper
+
+* 動作環境
+python2.7
+windowsではread関数で文字コードが勝手に変換されて、
+意図通り動作しないことが確認されています。
+
--- satloke_scraper.py
+++ satloke_scraper.py
@@ -1,16 +1,59 @@
 # coding: UTF-8
 
+import os
 import re
+import urllib
 import urllib2
 from bs4 import BeautifulSoup
 
+values = {'btn_download' : '',
+          'x' : '0',
+          'y' : '0'}
+# for team download
+#mode = 'team'
+#url = 'http://satloke.jp/datadetail.php?type=1&id='
+#last_number = 10023
+#last_number = 100
 
-html = urllib2.urlopen("http://satloke.jp/datadetail.php?type=1&id=10023")
+# for match download
+#mode = 'match'
+#url = 'http://satloke.jp/datadetail.php?type=2&id='
+#last_number = 372
 
-soup = BeautifulSoup(html,"lxml")
+# for replay download
+mode = 'replay'
+url = 'http://satloke.jp/datadetail.php?type=3&id='
+last_number = 113
 
-td_list = soup.find_all("td", id="data",width="468")
+rep = re.compile(r"<[^>]*?>")
 
-for td in td_list:
-	msg = td.extract().encode('Shift_JIS')
-	print(msg)
+os.mkdir(mode)
+
+for id in range(1, last_number+1):
+
+	html = urllib2.urlopen(url+str(id))
+	
+	soup = BeautifulSoup(html,"lxml")
+
+	td_list = soup.find_all("td", id="data",width="468")
+
+	if len(td_list) == 0:
+		print str(id)+"is none!!!!"
+		continue
+
+	msg = td_list[0].extract().encode('Shift_JIS')
+
+	file_name = rep.sub("", msg)
+	print(file_name)
+	
+	data = urllib.urlencode(values)
+	req = urllib2.Request(url, data)
+	response = urllib2.urlopen(req)
+	binaries = response.read()
+
+	os.mkdir(mode+"\\"+str(id))
+
+	f = open(mode+"\\"+str(id)+"\\"+file_name,"w")
+	f.write(binaries)
+	f.close()
+
